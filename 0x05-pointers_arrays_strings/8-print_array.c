90ba933dac5db608cbbe9baba8109912c64d9e85
Carnot <carnotfrancobgenge@gmail.com>
2023-08-21 12:40:49 -0700

    Edit 8-print_array.c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints element of array
+ * print_array - Prints elements of an array
  * @a: array name
- * @n: elements of the array
+ * @n: number of elements in the array
  *
  * Return: Always void.
  */
-void Print_array(int *a, int n)
+void print_array(int *a, int n)
 {
-	int i = 0;
+        int i = 0;
 
-	for (i = 0; i < n; i++)
-	{
-		if (i != (n - 1))
-		{
-			printf("%d, ", a[i]);
-		}
-		else
-		{
-			printf("%d", a[i]);
-		}
-	}
-	printf('\n');
+        for (i = 0; i < n; i++)
+        {
+                if (i != (n - 1))
+                {
+                        printf("%d, ", a[i]);
+                }
+                else
+                {
+                        printf("%d", a[i]);
+                }
+        }
+        printf("\n"); // Changed single quotes to double quotes here
 }
