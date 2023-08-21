a60ef83c24ec5f223949973a59b3420186d75a63
Carnot <carnotfrancobgenge@gmail.com>
2023-08-21 13:16:38 -0700

    Add 8-print_array.c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,15 @@
  */
 void print_array(int *a, int n)
 {
-    int i = 0;
+    int i;
 
     for (i = 0; i < n; i++)
     {
-        if (i != (n - 1))
+        printf("%d", a[i]);
+
+        if (i < n - 1) // Print comma and space for all elements except the last one
         {
-            printf("%d, ", a[i]);
-        }
-        else
-        {
-            printf("%d", a[i]);
+            printf(", ");
         }
     }
     printf("\n");
